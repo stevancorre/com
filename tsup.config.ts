07395353ee40f0d1e3dd799793fc12eaf8fe42f3
Stévan Corre <stevancorre@protonmail.com>
2022-06-05 15:23:56 +0200

    typescript and sass setup

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    name: "tsup",
+    target: "node16",
+    entry: ["src/ts/app.ts"],
+    minify: true,
+    sourcemap: true,
+    outDir: "dist"
+});
