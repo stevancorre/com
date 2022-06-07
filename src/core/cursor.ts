dce4de569e626d482442ba64331d1c930c99fb1d
Stévan Corre <stevancorre@protonmail.com>
2022-06-07 15:36:55 +0200

    fix cursor position at first mouse move

--- src/core/cursor.ts
+++ src/core/cursor.ts
@@ -9,7 +9,7 @@ export class Cursor {
     private dot: HTMLElement;
     private outline: HTMLElement;
 
-    private dotPosition: Vector2 = new Vector2();
+    private dotPosition: Vector2 = new Vector2(-1, -1);
     private outlinePosition: Vector2 = new Vector2();
 
     public constructor() {
@@ -26,6 +26,18 @@ export class Cursor {
     public render(): void {
         if (this.mousePosition === undefined) return;
 
+        // if last position is unknown
+        if (this.dotPosition.equals(new Vector2(-1, -1))) {
+            this.dotPosition = this.mousePosition.clone();
+            this.outlinePosition = this.mousePosition.clone();
+        } else {
+            this.calculateNextPositions();
+        }
+
+        this.updatePositions();
+    }
+
+    private calculateNextPositions(): void {
         const cursorDiff: Vector2 = this.mousePosition
             .clone()
             .sub(this.dotPosition);
@@ -43,7 +55,9 @@ export class Cursor {
                     config.cursor.outlineSpeed,
             ),
         );
+    }
 
+    private updatePositions(): void {
         this.dot.style.left = this.dotPosition.x + "px";
         this.dot.style.top = this.dotPosition.y + "px";
 
