eab123f37743a7e0a8c0a08cd8c8330abc3e3ad4
Stévan Corre <stevancorre@protonmail.com>
2022-06-08 19:03:15 +0200

    remove useless index in core

--- src/core/index.ts
+++
@@ -1,6 +0,0 @@
-import { Renderer } from "./renderer";
-import { Camera } from "./camera";
-import { Scene } from "./scene";
-import { Stats } from "./stats";
-
-export { Renderer, Camera, Scene, Stats };
